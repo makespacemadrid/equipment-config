79a666d35081b43cf85a314482c0f533284f4658
Javi <darkjavi@gmail.com>
2016-02-02 22:35:19 +0100

    Reducida la velocidad Z por si acaso

--- Luzlbot-3DPrinter/Firmware/1.0.2MM/Marlin/Configuration.h
+++ Luzlbot-3DPrinter/Firmware/1.0.2MM/Marlin/Configuration.h
@@ -478,7 +478,7 @@ const bool Z_MAX_ENDSTOP_INVERTING = true; // set to true to invert the logic of
 // default settings
 
 #define DEFAULT_AXIS_STEPS_PER_UNIT   {80.20,80.20,4020,460}  // default steps per unit for Ultimaker
-#define DEFAULT_MAX_FEEDRATE          {500, 500, 3, 35}    // (mm/sec)
+#define DEFAULT_MAX_FEEDRATE          {500, 500, 2.5, 35}    // (mm/sec)
 #define DEFAULT_MAX_ACCELERATION      {9000,9000,100,10000}    // X, Y, Z, E maximum start speed for accelerated moves. E default values are good for Skeinforge 40+, for older versions raise them a lot.
 
 #define DEFAULT_ACCELERATION          2500    // X, Y, Z and E max acceleration in mm/s^2 for printing moves
